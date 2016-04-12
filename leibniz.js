4c9ba1293c77155e4e3243100b50588ce72b3c3f
Tyler James Bounds <tyler.bounds@wsu.edu>
2016-04-12 10:56:54 -0700

    timesOfRule

--- leibniz.js
+++ leibniz.js
@@ -198,7 +198,7 @@ var unityRule = {
 var times0Rule = {
     pattern: function(target, table) {
       return smatch(['*', 'E?', 'N?'], target, table) &&
-        (typeof table.E === "number" && typeof table.N === "string") || (typeof table.E === "string" && typeof table.N === "numebr") 
+        ((typeof table.E === 0 && typeof table.N === "string") || (typeof table.E === "string" && table.N === 0)) 
     },
     transform: function(table) {
         return 0;
